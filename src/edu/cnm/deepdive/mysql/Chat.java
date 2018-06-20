d782b284da97174c7fbf9e6d5ffe5f6d96cf4a4e
Jason Rafferty <Jason0x55@gmail.com>
2018-06-20 09:03:03 -0600

    Initial commit for mysql example.

---
+++ src/edu/cnm/deepdive/mysql/Chat.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.mysql;
+
+public class Chat {
+
+  public static void main(String[] args) {
+    DatabaseHelper databaseHelper = new DatabaseHelper();
+    for (Message message: databaseHelper.readMessages()) {
+      System.out.println(message.getMessage());
+    }
+  }
+
+}
